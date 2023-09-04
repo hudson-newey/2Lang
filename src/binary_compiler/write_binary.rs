e2cc7e2ff301a2ff68c8d92b1d6162ce10eddefd
hudson-newey <hudsonnewey@gmail.com>
2023-09-04 16:10:08 +1000

    Got hello world working

--- src/binary_compiler/write_binary.rs
+++ src/binary_compiler/write_binary.rs
@@ -1,5 +1,5 @@
 use std::fs::File;
-use std::io::Write;
+use std::io::{self, Write};
 use std::os::unix::fs::PermissionsExt;
 
 pub fn delete_file(file_path: String) {
@@ -12,33 +12,10 @@ pub fn delete_file(file_path: String) {
 }
 
 pub fn write_binary(binary: Vec<u8>) {
-    let mut full_binary: Vec<u64> = Vec::new();
-    let mut i: u64 = 0;
-    let mut j: u16 = 0;
+    let mut full_binary: Vec<bool> = Vec::new();
 
-    full_binary.push(0);
-
-    let reversed_binary: Vec<u8> = binary
-        .iter()
-        .rev()
-        .cloned()
-        .collect();
-
-    for bit in reversed_binary {
-        if j >= 64 {
-            i += 1;
-            j = 0;
-
-            full_binary.push(0);
-        }
-
-        if bit == 0 {
-            full_binary[i as usize] &= !(1 << j);
-        } else if bit == 1 {
-            full_binary[i as usize] |= 1 << j;
-        }
-
-        j += 1;
+    for bit in binary {
+        full_binary.push(bit == 1);
     }
 
     println!();
@@ -49,14 +26,41 @@ pub fn write_binary(binary: Vec<u8>) {
     make_file_executable("a.out".to_string());
 }
 
-fn write_file(file_name: String, binary: Vec<u64>) {
-    let mut file = File::create(file_name)
-        .expect("Something went wrong creating the file");
+fn write_file(file_path: String, binary: Vec<bool>) -> io::Result<()> {
+    // Create a file for writing.
+    let mut file = File::create(file_path)?;
 
-    for byte in binary {
-        file.write_all(&byte.to_ne_bytes())
-            .expect("Something went wrong writing the file");
+    // Create a buffer to store the bits.
+    let mut buffer: u8 = 0;
+    let mut buffer_position = 0;
+
+    // Iterate through the binary and pack them into bytes.
+    for bit in binary {
+        // Shift the current bits in the buffer to the left by one.
+        buffer <<= 1;
+
+        // Set the least significant bit to 1 if the bool is true.
+        if bit {
+            buffer |= 1;
+        }
+
+        // Increment the buffer position.
+        buffer_position += 1;
+
+        // If the buffer is full (8 bits), write it to the file and reset the buffer.
+        if buffer_position == 8 {
+            file.write_all(&[buffer])?;
+            buffer = 0;
+            buffer_position = 0;
+        }
     }
+
+    // If there are remaining bits in the buffer, write them to the file.
+    if buffer_position > 0 {
+        file.write_all(&[buffer])?;
+    }
+
+    Ok(())
 }
 
 fn make_file_executable(file_path: String) {
